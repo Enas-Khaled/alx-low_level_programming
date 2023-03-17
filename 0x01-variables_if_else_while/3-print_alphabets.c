f54ee09d617318609ba488dd4ed319ff6d5d2434
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-17 07:16:52 -0700

    a to z, A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	for (c = 65; c <= 90; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
