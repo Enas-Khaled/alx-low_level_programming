ad39a752aaaa780d8b7ca7542325bd8736d82e90
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-21 14:06:58 -0700

    void print_alphabet(void)

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-void print_alphabet(void);
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
  *print_alphabet - prints a to z
  *
