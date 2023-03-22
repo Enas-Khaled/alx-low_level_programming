d6e3c7d34b1f43e032c6dbb064646d8365e0d331
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-22 16:31:50 -0700

    task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entery point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int first = 1, second = 2, next, i;
+
+	printf("%d, %d, ", first, second);
+	for (i = 2; i < 50; i++)
+	{
+		next = first + second;
+		printf("%d", next);
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		first = second;
+		second = next;
+	}
+	printf("\n");
+	return (0);
+}
