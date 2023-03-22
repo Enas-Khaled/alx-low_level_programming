eea49e4188f3738cf1d67f65e4bca1ed3acdbb75
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-22 16:58:15 -0700

    fix

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entery point
+ * Return: void
+ */
+int main(void)
+{
+	int fib1 = 1;
+	int fib2 = 2;
+
+	printf("%d, %d, ", fib1, fib2);
+	for (int i = 0; i < 96; i++)
+	{
+		int fib_next = fib1 + fib2;
+		printf("%d, ", fib_next);
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+	printf("\n");
+	return (0);
+}
