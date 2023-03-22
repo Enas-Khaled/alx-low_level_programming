87b015b97b2c47b4fd41ea66c1ec51a7930daee4
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-22 09:05:29 -0700

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - Prints time tabls from 0 to n
+ * @n: integer
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i = 0, j, n1, n2, res;
+
+	if ((n > 15) || (n < 0)
+		_putchar('');
+	else
+	{
+		while (i < n)
+		{
+			j = 0;
+			while (j < n)
+			{
+				res = i * j;
+
+				if (res > 9)
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					n1 = res / 10;
+					n2 = res % 10;
+					_putchar(n1 + '0');
+					_putchar(n2 + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(res + '0');
+				}
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
