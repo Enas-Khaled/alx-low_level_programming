01283d7e099aee7b307b334da37d5d299142a69e
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-23 15:42:46 -0700

    print squar

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - void print_diagonal
+ * @n: no of \
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
