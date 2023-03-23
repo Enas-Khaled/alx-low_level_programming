335d5de35e0e14ad034e0a346e535ab4a9bcf2ad
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-23 04:52:41 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if it is upper
+ * @c: char to chech
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if(isupper(c))
+		return (1);
+	else
+		return (0);
+}
+
