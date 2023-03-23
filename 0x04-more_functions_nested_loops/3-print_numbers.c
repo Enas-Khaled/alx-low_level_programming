1f57bab5985ab57048b085b5e0504e3cb44372f3
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-23 14:02:07 -0700

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - print nums
@@ -16,3 +15,4 @@ void print_numbers(void)
 	}
 	_putchar('\n');
 }
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include "_putchar.c"
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
