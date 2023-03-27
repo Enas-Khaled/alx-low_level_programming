38e65273d8b1b0c9be3f0a30ef754cf4f069f20a
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-27 15:18:41 -0700

    void print_rev(char *s);

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - void print_rev(char *s);
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, j = 0;
+
+	while (s[j] != '\0')
+		j++;
+	for (i = j; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
