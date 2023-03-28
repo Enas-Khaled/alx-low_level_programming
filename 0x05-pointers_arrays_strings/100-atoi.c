4a08542b8465f92b75b437ba004f3cab902f9e9d
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-28 01:13:03 -0700

    int _atoi(char *s);

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,12 @@
 #include "main.h"
 int _atoi(char *s) {
     int sign = 1;
-    int num = 0;
+    unsigned int num = 0;
     int seen_digit = 0;
 
     while (*s != '\0') {
         if (*s == '-' && !seen_digit) {
-            sign = -1;
-        } else if (*s == '+' && !seen_digit) {
-            sign = 1;
+            sign*= -1;
         } else if (*s >= '0' && *s <= '9') {
             seen_digit = 1;
             num = num * 10 + (*s - '0');
