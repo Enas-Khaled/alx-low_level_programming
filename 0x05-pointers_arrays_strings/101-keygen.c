ae089a1f885eab4d005e3b5258365e61f63ddf8a
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-28 01:49:31 -0700

    random pss

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entery point
+ * Return: void
+ */
+int main(void)
+{
+    int i;
+    char password[7];
+    srand(time(NULL));
+    const char *const valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    printf("Generating random passwords for 101-crackme...\n");
+    
+    for (i = 0; i < 10; i++) {
+        int j;
+        for (j = 0; j < 6; j++) {
+            int r = rand() % 62;
+            password[j] = valid_chars[r];
+        }
+        password[6] = '\0';
+        printf("Password %d: %s\n", i+1, password);
+    }
+    return 0;
+}
+
