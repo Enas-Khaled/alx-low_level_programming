bee6e85e0a84523dc661362d5a7c41b11e94a2a1
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-29 02:01:03 -0700

    char *_strncpy(char *dest, char *src, int n);

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strncpy - copy(char *dest, char *src, int n)
+ * @dest: destination
+ * @src:source
+ * @n: size
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && i != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
