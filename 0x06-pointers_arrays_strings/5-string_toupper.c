2ec4ba4af74abe0754fb2500592543abfa569299
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-29 23:34:20 -0700

    fix 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,13 +4,15 @@
  * @str: string
  * Return: char
  */
-char* string_toupper(char* str) {
-    char* p = str;
-    while (*p) {
-        if (*p >= 'a' && *p <= 'z') {
-            *p = *p - ('a' - 'A');
-        }
-        p++;
-    }
-    return str;
+char* string_toupper(char* str)
+{
+	char* p = str;
+	
+	while (*p)
+	{
+		if (*p >= 'a' && *p <= 'z')
+			*p = *p - ('a' - 'A');
+		p++;
+	}
+	return str;
 }
