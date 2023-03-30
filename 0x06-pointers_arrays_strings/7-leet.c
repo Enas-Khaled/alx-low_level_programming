19acd7e0843bc5f9e05a8f2dc9ebca2bd8d17226
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-30 00:50:59 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *leet - encode(char *)
+ * @str: string
+ * Return: char
+ */
+char *leet(char *str)
+{
+	char *p = str;
+
+	while (*p)
+	{
+		if (*p == 'a' || *p == 'A')
+		       	*p = '4';
+        else if (*p == 'e' || *p == 'E')
+            *p = '3';
+        else if (*p == 'o' || *p == 'O')
+            *p = '0';
+        else if (*p == 't' || *p == 'T')
+            *p = '7';
+        else if (*p == 'l' || *p == 'L')
+            *p = '1';
+        p++;
+    }
+    return str;
+}
