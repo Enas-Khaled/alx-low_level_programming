b3156e717349e7329066084eea70f9a91ace8234
Enas Khaled <enas.ah.khaled@gmail.com>
2023-03-30 01:04:17 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,16 +11,16 @@ char *leet(char *str)
 	while (*p)
 	{
 		if (*p == 'a' || *p == 'A')
-		       	*p = '4';
-        else if (*p == 'e' || *p == 'E')
-            *p = '3';
-        else if (*p == 'o' || *p == 'O')
-            *p = '0';
-        else if (*p == 't' || *p == 'T')
-            *p = '7';
-        else if (*p == 'l' || *p == 'L')
-            *p = '1';
-        p++;
-    }
-    return str;
+			*p = '4';
+		else if (*p == 'e' || *p == 'E')
+			*p = '3';
+		else if (*p == 'o' || *p == 'O')
+			*p = '0',
+		else if (*p == 't' || *p == 'T')
+			*p = '7';
+		else if (*p == 'l' || *p == 'L')
+			*p = '1';
+		p++;
+	}
+	return (str);
 }
