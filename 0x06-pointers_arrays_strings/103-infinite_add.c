920694234f1f0835af4c29c43f75669d8f7ae398
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-01 06:33:30 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,10 +21,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	*p = '\0';
 	while (i >= 0 || j >= 0 || carry > 0)
 	{
-		sum = curry;
+		sum = carry;
 		if (i >= 0)
 			sum += n1[i--] - '0';
-		if (y >= 0)
+		if (j >= 0)
 			sum += n2[j--] - '0';
 		carry = sum / 10;
 		*--p = (sum % 10) + '0';
