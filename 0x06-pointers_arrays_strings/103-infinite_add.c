5bdc626093b47de221121df0bda1dac48b7c12b8
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-01 06:52:40 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,24 +10,35 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
-	int len2 = strlen(n2);
+    int len2 = strlen(n2);
+    int lenr = size_r - 1;  // Leave room for null terminator
+    int carry = 0;
+    int i, j;
 
-	if (len1 + len2 >= size_r)
-		return (0);
+    // Make sure the result buffer is large enough
+    if (len1 + len2 > lenr) {
+        return 0;
+    }
 
-	int carry = 0, sum = 0, i = len1 - 1, j = len2 - 1;
-	char *p = r + size_r - 1;
+    // Start adding digits from the right
+    i = len1 - 1;
+    j = len2 - 1;
+    while (i >= 0 || j >= 0 || carry > 0) {
+        int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+        int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+        int sum = digit1 + digit2 + carry;
+        if (sum >= 10) {
+            carry = 1;
+            sum -= 10;
+        } else {
+            carry = 0;
+        }
+        r[--lenr] = sum + '0';
+        i--;
+        j--;
+    }
 
-	*p = '\0';
-	while (i >= 0 || j >= 0 || carry > 0)
-	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i--] - '0';
-		if (j >= 0)
-			sum += n2[j--] - '0';
-		carry = sum / 10;
-		*--p = (sum % 10) + '0';
-	}
-	return (p);
+    // Add null terminator and return pointer to result
+    r[size_r - 1] = '\0';
+    return &r[lenr];
 }
