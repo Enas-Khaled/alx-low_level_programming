6354b34c3e200d07f3e6d41bd8d3aee30c5fb7eb
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-03 19:51:59 -0700

    searsh

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strchr - searsh of c in s(char *s, char c)
+ * @s: string
+ * @c: char
+ * Return: NULL or pointer of c position
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i <= strlen(s); i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (NULL);
+}
