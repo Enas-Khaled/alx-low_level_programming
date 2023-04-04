27c28f8470968824385d57a6b9eb8a5dc039121a
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-03 18:36:27 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_memset - fill memory s with n size by b(char *s, char b, unsigned int n)
+ * @s: string
+ * @b: element
+ * @n: size
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
