5b22d41d844513c17f435df30ce5a5822d820c42
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-05 16:18:28 -0700

    void _puts_recursion(char *s);

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s)
-		_putchar(*s++);
-	_putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
