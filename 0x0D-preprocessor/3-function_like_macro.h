4354e4e02e06ece2bdaa0f87a84d6221a347b6f8
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-18 17:11:53 -0700

    _FUNCTION_LIKE_MACRO_H

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABC(X) ((X > 0) ? (X) : (-X))
+#define ABC(X) ((X > 0) ? (X) : (-(X)))
 
-#endif
+#endif /* _FUNCTION_LIKE_MACRO_H */
