1af68e08581ce60f00b683447a82c8e62ade0012
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-19 22:40:41 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * main - entery point
+ * @argc: nums of arg
+ * @argv: vector of arg
+ * Return: 0 
+ */
+int main (int argc, char **argv)
+{
+	int (*op_fun)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_fun = get_op_func(argv[2]);
+	if (!op_fun)
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+
+	printf("%d\n",op_fun(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - (int a, int b)
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - (int a, int b)
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - (int a, int b)
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - (int a, int b)
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - (int a, int b)
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
