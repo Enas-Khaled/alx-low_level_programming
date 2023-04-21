ad92b1328e51eb521cdab7375e8bcafae23409aa
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-20 17:50:43 -0700

    separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - (const char *separator, const unsigned int n, ...);
+ * @n: counts
+ * @separator: sep
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+	char *str;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(ap, n);
+
+	while (i--)
+		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+
+	va_end(ap);
+}
