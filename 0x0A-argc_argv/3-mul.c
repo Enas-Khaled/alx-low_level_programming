9b7c04cf75d6529ea6e378446fc9acb3ab7ddb24
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-26 01:03:40 -0700

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entery point
+ * @argc: size
+ * @argv: values
+ * Return: 0 success
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
