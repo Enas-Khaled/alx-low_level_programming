ac3c7be40c4b91e0433ead7b517bb94db9e12aa8
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-26 00:36:53 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ * @argc: size
+ * @argv: vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
