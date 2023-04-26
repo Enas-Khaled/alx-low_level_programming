101a3bd513241fe4043e92acc290320b3a047131
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-26 01:27:12 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - entery point
+ * @argc: size
+ * @argv: values
+ * Return: 0 success
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+		j = 0;
+		while (arg[j] != '\0')
+		{
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += atoi(arg);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
