b0c3ef09b001d4f681b8731a6f92c560dbc70374
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-26 00:43:52 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ * @argc: size
+ * @argv: values
+ * Return: 0 success
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
