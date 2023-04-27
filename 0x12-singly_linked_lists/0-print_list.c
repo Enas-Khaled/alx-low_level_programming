570ece8756b022d11f253260d503d5923509f67c
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-27 12:01:51 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,20 @@
 #include "lists.h"
+#include "lists.h"
+/**
+ * _strlen - count len
+ * @s: string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
 /**
  * print_list - (const list_t *h);
  * @h: list
