66976a1884a7239df508eeba998639abfb8e4c6e
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-27 00:35:49 -0700

    create-array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * create_array - malloc array
+ * @size: array size
+ * @c: char to init with
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = (char*)malloc(size * sizeof(char));
+
+	if (size == 0 || ptr == 0)
+		return (0);
+	while (size--)
+		ptr[size] = c;
+	return (ptr);
+}
