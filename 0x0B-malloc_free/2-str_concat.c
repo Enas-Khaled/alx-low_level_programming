82e73a939f31596217cf5400a10866f0b5ec3a29
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-27 01:15:26 -0700

    conctAraays

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * str_concat - (char *s1, char *s2)
+ * @s1: first str
+ * @s2: second str
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0, i, j;
+	char *ptr;
+
+	while (s1[size1] != '\0')
+		size1++;
+	while (s2[size2] != '\0')
+		size2++;
+
+	ptr = malloc(size1 + size2 + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; i < (size1 + size2); i++, j++)
+		ptr[i] = s2[j];
+	return (ptr);
+}
