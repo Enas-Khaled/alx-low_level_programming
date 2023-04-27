fa43c9eae2280508c9014ec7c29e40ecbf8f4791
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-27 00:37:07 -0700

    create-array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *ptr = (char*)malloc(size * sizeof(char));
+	char *ptr = (char *)malloc(size * sizeof(char));
 
 	if (size == 0 || ptr == 0)
 		return (0);
