33b3736872338f6f5667265f04c0a3d66a494569
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-27 13:26:22 -0700

    try assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,14 @@
-section		.text
-	extern	printf
-	global	main
+section .data
+    hello: db 'Hello, Holberton,', 10, 0
+
+section .text
+    global main
+
 main:
-	mov	edi, msg
-	mov	eax, 0
-	call	printf
-section		.data
-	msg db 'Hello, Holberton,', 0xa, 0
+    mov rax, 0                  ; clear rax register
+    lea rdi, [hello]            ; load address of hello string into rdi
+    xor esi, esi                ; set esi register to 0 (no floating point arguments)
+    call printf                 ; call printf to print the string
+    xor eax, eax                ; set return value to 0
+    ret                         ; return to operating system
+
