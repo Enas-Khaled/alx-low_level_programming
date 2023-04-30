72817cc8595f5403dd9a4f30e36280b2f85a50a1
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-30 12:57:16 -0700

    FreeAgain

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
+		return;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		temp = head;
-		head = head->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
 	}
 	*head = NULL;
