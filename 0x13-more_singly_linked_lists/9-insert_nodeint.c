b89ff9d805825842045d6a04d9f8e81b6fa6b428
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-30 23:37:49 -0700

    frist_try_to_insert_Node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,8 @@
  * insert_nodeint_at_index - insert node
  * @idx: index
  * @n: value
+ * @head: pointer to head
+ * Return: new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -11,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-	
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
