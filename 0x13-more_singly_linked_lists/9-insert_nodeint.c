3c7ca9d019e7f9865b756383a83ade179e13f8e3
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-30 23:34:50 -0700

    frist_try_to_insert_Node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,13 +9,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 	listint_t *new, *temp;
 
+	if (head == NULL)
+		return (NULL);
+	
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || *head == NULL || head == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
 
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (*head != NULL)
 	{
 		if (count == idx)
