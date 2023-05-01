b1995b94b8f511bd22b29d9f6dd849e5a15c34ca
Enas Khaled <enas.ah.khaled@gmail.com>
2023-04-30 23:07:47 -0700

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * sum_listint - adds n values
+ * @head: head
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+		sum += head->n;
+	return (sum);
+}
