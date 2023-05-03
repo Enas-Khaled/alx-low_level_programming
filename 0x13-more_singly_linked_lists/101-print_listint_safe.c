2a1abe4979697615ac63f0444772b8f0eba2da9d
Enas Khaled <enas.ah.khaled@gmail.com>
2023-05-02 17:46:48 -0700

    print_safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint_safe - (const listint_t *head);
+ * @head: head
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+
+	if (head == NULL)
+		exit(98);
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->next);
+		head = head->next;
+		count++;
+	}
+	return (counts);
+}
