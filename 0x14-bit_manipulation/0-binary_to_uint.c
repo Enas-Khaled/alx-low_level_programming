94104b9c87ba660369d921f520cd24cc3d244141
Enas Khaled <enas.ah.khaled@gmail.com>
2023-05-04 16:59:56 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,9 +13,9 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while(*b != NULL)
+	while(*b != '\0')
 	{
-		if (b != 0 && b != 1)
+		if (*b != '0' && *b != '1')
 			return (0);
 		res = (res << 1) + (*b - '0');
 		b++;
