904768306b73323cea2c4e07503859d41d757d95
Enas Khaled <enas.ah.khaled@gmail.com>
2023-05-04 19:22:02 -0700

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,13 +12,13 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 
 	mask = 1UL << index;
 
 	*n &= ~mask;
 
-	return 1;
+	return (1);
 }
 
