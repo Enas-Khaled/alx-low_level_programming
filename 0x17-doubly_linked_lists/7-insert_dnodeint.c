f621eac8d8b15d890c323f36b5d2b050e8fcc874
Enas Khaled <enas.ah.khaled@gmail.com>
2023-11-09 18:48:57 -0800

    add node in given index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * *insert_dnodeint_at_index - (dlistint_t **h, unsigned int idx, int n)
+ * @h: ptr to head
+ * @idx: index to new node
+ * @n: data to add
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = *h;
+	unsigned int count = 0;
+
+	if (!h || !new || !n)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	if (idx == 0)
+		return(*add_dnodeint(h, n));
+	else
+	{
+		while (temp)
+		{
+			if (count == idx - 1)
+			{
+				new->n = n;
+				new->next = temp->next;
+				new->prev = temp;
+				if (temp->next)
+					temp->next->prev = new;
+				temp->next = new;
+				return (new);
+			}
+			count += 1;
+			temp = temp->next;
+
+		}
+		free(new);
+		return (add_dnodeint_end(h, n));
+	}
+	free(new);
+	return (NULL);
+}
