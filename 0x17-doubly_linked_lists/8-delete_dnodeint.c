ed97bba7e02eb1e78295c418e31ce0de7134a1fb
Enas Khaled <enas.ah.khaled@gmail.com>
2023-11-09 19:19:27 -0800

    test

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - (dlistint_t **head, unsigned int index)
+ * @head: ptr
+ * @index: indx
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ * 
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current  = *head;
+	unsigned int count = 0;
+
+	if (!head || !*head)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	while (current && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (!current)
+	return (-1);
+
+	if (current->prev)
+		current->prev->next = current->next;
+	if (current->next)
+		current->next->prev = current->prev;
+
+	free(current);
+	return (1);
+
+}
