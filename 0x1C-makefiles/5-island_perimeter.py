a3944dab703785063a06267b625c3cc561344d14
Enas Khaled <enas.ah.khaled@gmail.com>
2024-02-22 03:41:20 -0800

    gridIsland

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/bin/usr/env python3
+def island_perimeter(grid):
+    I = len(grid)
+    J = len (grid[0])
+    count = 0
+    for i in range(I):
+        for j in range(J):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0: count += 1
+                if j == 0 or grid[i][j - 1] == 0: count += 1
+                if i == I - 1 or grid[i + 1][j] == 0: count += 1
+                if j == J - 1 or grid[i][j + 1] == 0: count += 1
+    return count
